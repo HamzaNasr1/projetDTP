43daff40b2abf762c2f7a51281e0e3ee121cec10
hamza_nasr <nasr.hamza@esprit.tn>
2025-11-24 11:32:38 +0100

    set -e affichage

--- Scripts/scraping.py
+++ Scripts/scraping.py
@@ -1,23 +1,12 @@
-# scraper.py
-
 import requests
 from bs4 import BeautifulSoup
 
-# URL du site à scraper
+
 url = "http://quotes.toscrape.com/"
-
-# Envoyer la requête GET
 response = requests.get(url)
-
-# Vérifier si la requête a réussi
 if response.status_code == 200:
-    # Parser le contenu HTML
     soup = BeautifulSoup(response.text, "html.parser")
-    
-    # Extraire la première citation
     first_quote = soup.find("span", class_="text").get_text()
-    
-    # Afficher la citation
-    print("Première citation trouvée :", first_quote)
+    print("première citation :", first_quote)
 else:
-    print("Erreur lors de la requête :", response.status_code)
+    print("eerreur.. ", response.status_code)
